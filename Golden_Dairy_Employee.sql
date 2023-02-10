8502ebdbfa404a805830bfb48c5bf9b8b56cb30d
2021514 <107923239+2021514@users.noreply.github.com>
2023-02-10 19:05:13 +0530

    Add files via upload

--- Golden_Dairy_Employee.sql
+++ Golden_Dairy_Employee.sql
@@ -23,13 +23,14 @@ DROP TABLE IF EXISTS `Employee`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Employee` (
-  `ID` int PRIMARY KEY NOT NULL,
+  `ID` int NOT NULL,
   `Name` varchar(50) DEFAULT NULL,
   `Address` varchar(100) DEFAULT NULL,
   `Email` varchar(50) DEFAULT NULL,
   `Contact_No` varchar(50) DEFAULT NULL,
   `Salary` int DEFAULT NULL,
-  `Role` varchar(50) DEFAULT NULL
+  `Role` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -52,5 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-09 18:20:03
-select * from Employee;
+-- Dump completed on 2023-02-10 19:03:52
